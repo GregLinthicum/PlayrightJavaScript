4251b02238159a0aa54856d9a6d1190108b88caf
GregLinthicum <52845178+GregLinthicum@users.noreply.github.com>
2024-07-04 09:43:35 -0400

    next day after reboot commit one

--- jsTests/example.spec.js
+++ jsTests/example.spec.js
@@ -7,7 +7,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-////////  git push -u playrightjsrepoB master  ..
+////////  git push -u playrightjsrepoB master  .. .
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
